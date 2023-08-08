1bae6459ec27545a5500ca46b3127bb6bb911a7e
21RABIN <104710006+21RABIN@users.noreply.github.com>
2023-08-08 22:41:35 +0530

    Add files via upload

---
+++ Week_2/Exercise2_3.java
@@ -0,0 +1,28 @@
+package Week_2;
+
+//This is the main class Question
+public class Exercise2_3 {
+	public static void main(String[] args) {
+		// Object of the main class is created
+		Exercise2_3 q = new Exercise2_3();
+		// Print method on object of Question class is called
+		q.studentMethod();
+	}
+
+	// 'print()' method is defined in class Question
+	void print(Exercise2_3 object) {
+		System.out.print("Well Done!");
+	}
+
+	// Define a method named 'studentMethod()' in class Question
+	void studentMethod() {
+
+		// Call the method named 'print()' in class Question
+		Exercise2_3 q = new Exercise2_3();
+		q.print(q);
+	}
+}
+
+/*---------OUTPUT-----------
+ * Well Done!
+--------------------------*/
